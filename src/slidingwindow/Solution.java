6b8f109b29c087e4b12c4571316f852373ce3289
Karthikeyan010 <114061543+Karthikeyan010@users.noreply.github.com>
2026-09-26 11:19:09 +0100

    sliding window by using the map

--- src/slidingwindow/Solution.java
+++ src/slidingwindow/Solution.java
@@ -224,4 +224,25 @@ public class Solution {
 
         return maxLength;
     }
+
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> freq= new HashMap<>();
+
+        for(int num : nums){
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+        }
+
+        int max=0;
+
+        for(int num : freq.keySet()){
+            if(freq.containsKey(num+1)){
+                int length= freq.get(num)+freq.get(num+1);
+
+                max= Math.max(max, length);
+            }
+        }
+
+        return max;
+
+    }
 }
